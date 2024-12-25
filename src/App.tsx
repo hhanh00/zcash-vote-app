6abba2a9c1fa47561f8d40a3a12192ac5393a62b
Hanh <hanh425@gmail.com>
2024-12-25 22:26:49 +1000

    feat: scaffolding

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "flowbite-react/components/Button";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="container">
+      <h1>Welcome to Tauri + React</h1>
+      <Button>Click me</Button>
+    </main>
+  );
+}
+
+export default App;
