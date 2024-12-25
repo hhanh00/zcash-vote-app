293cd9bedf5c926f925ef8788bdc093bd994bf65
Hanh <hanh425@gmail.com>
2024-12-25 22:37:39 +1000

    feat: new election

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,22 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use std::sync::Mutex;
+
+use state::AppState;
+use zcash_vote::Election;
+
+pub mod state;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(Mutex::new(AppState {
+            election: Election::default(),
+        }))
+        .invoke_handler(tauri::generate_handler![
+            state::set_election,
+            state::get_election,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
