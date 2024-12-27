eeca7a904a97cbf9d73df3688ff8b59b375c2c20
hhanh00 <hanh425@gmail.com>
2024-12-27 11:05:21 +1000

    feat: ask and store seed/ufk (#2)

--- src-tauri/src/state.rs
+++ src-tauri/src/state.rs
@@ -12,6 +12,7 @@ use crate::db::{create_schema, load_election, store_election};
 pub struct AppState {
     pub url: String,
     pub election: Election,
+    pub key: String,
     pub pool: r2d2::Pool<SqliteConnectionManager>,
 }
 
@@ -20,6 +21,7 @@ impl Default for AppState {
         Self {
             url: Default::default(),
             election: Default::default(),
+            key: Default::default(),
             pool: Pool::new(SqliteConnectionManager::memory()).unwrap(),
         }
     }
@@ -37,7 +39,7 @@ pub fn save_db(path: String, state: State<Mutex<AppState>>) -> Result<(), String
         let manager = SqliteConnectionManager::file(&path);
         let pool = Pool::new(manager)?;
         let connection = pool.get()?;
-        store_election(&connection, &s.url, &s.election)?;
+        store_election(&connection, &s.url, &s.election, &s.key)?;
         s.pool = pool;
         Ok::<_, Error>(())
     })().map_err(|e| e.to_string())
@@ -49,19 +51,21 @@ pub fn open_db(path: String, state: State<Mutex<AppState>>) -> Result<(), String
         let mut s = state.lock().unwrap();
         let pool = Pool::new(SqliteConnectionManager::file(path))?;
         let connection = pool.get()?;
-        let (url, election) = load_election(&connection)?;
+        let (url, election, key) = load_election(&connection)?;
         s.url = url;
         s.election = election;
+        s.key = key;
         s.pool = pool;
         Ok::<_, Error>(())
     })().map_err(|e| e.to_string())
 }
 
 #[tauri::command]
-pub fn set_election(url: String, election: Election, state: State<Mutex<AppState>>) {
+pub fn set_election(url: String, election: Election, key: String, state: State<Mutex<AppState>>) {
     let mut s = state.lock().unwrap();
     s.url = url.clone();
     s.election = election;
+    s.key = key.clone();
 }
 
 #[tauri::command]
--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,18 @@ import {
   useNavigate,
   Navigate,
 } from 'react-router-dom'
-import "./App.css"
+import './App.css'
 import axios from 'axios'
 import { open, save } from '@tauri-apps/plugin-dialog'
 import { Overview } from './Overview'
-import { Button, Modal, TextInput } from 'flowbite-react'
+import { Button, Label, Modal, TextInput } from 'flowbite-react'
 import { Controller, SubmitHandler, useForm } from 'react-hook-form'
 import { useState } from 'react'
 import { invoke } from '@tauri-apps/api/core'
 
 type FormValues = {
   url: string;
+  key: string;
 }
 
 function Election() {
@@ -24,7 +25,8 @@ function Election() {
   const { control, handleSubmit, formState: { errors } } = useForm(
     {
       defaultValues: {
-        url: 'https://vote.zcash-infra.com/nsm-nu7'
+        url: 'https://vote.zcash-infra.com/nsm-nu7',
+        key: 'shadow emerge trouble police canal access evil loyal giant click night price mule just clutch math fossil curious trim denial cereal measure left slight',
       },
       mode: 'onSubmit'
     }
@@ -45,7 +47,7 @@ function Election() {
     const election = rep.data
 
     console.log(election)
-    await invoke('set_election', {url: data.url, election: election})
+    await invoke('set_election', {url: data.url, election: election, key: data.key})
     const name = election.name;
 
     (async () => {
@@ -76,56 +78,65 @@ function Election() {
 
   return (
     <>
-    <nav className="flex items-center justify-between px-8 py-2 bg-gray-800 text-white">
-      <a href="/home" className="hover:text-gray-400">Election</a>
-      <a href="/overview" className="text-gray-400">Overview</a>
-      <a href="/history" className="text-gray-400">History</a>
-      <button className="px-4 py-2 bg-blue-600 rounded hover:bg-blue-700">Vote</button>
+    <nav className='flex items-center justify-between px-8 py-2 bg-gray-800 text-white'>
+      <a href='/home' className='hover:text-gray-400'>Election</a>
+      <a href='/overview' className='text-gray-400'>Overview</a>
+      <a href='/history' className='text-gray-400'>History</a>
+      <button className='px-4 py-2 bg-blue-600 rounded hover:bg-blue-700'>Vote</button>
     </nav>
 
-    <div className="flex flex-grow items-center justify-center">
-      <div className="bg-white max-w-xl mx-auto rounded-lg shadow-lg p-8">
-        <h1 className="text-2xl font-bold text-gray-800 mb-4 text-center">Welcome to the Zcash Voting App</h1>
-        <p className="text-gray-600 text-center">
+    <div className='flex flex-grow items-center justify-center'>
+      <div className='bg-white max-w-xl mx-auto rounded-lg shadow-lg p-8'>
+        <h1 className='text-2xl font-bold text-gray-800 mb-4 text-center'>Welcome to the Zcash Voting App</h1>
+        <p className='text-gray-600 text-center'>
           Click on
-          <Button onClick={() => setOpenModal(true)} className="inline-flex mx-2">
+          <Button onClick={() => setOpenModal(true)} className='inline-flex mx-2'>
             New
           </Button>
           to start voting on a new election, or
-          <Button onClick={openDb} className="inline-flex mx-2">
+          <Button onClick={openDb} className='inline-flex mx-2'>
             Open
           </Button>
           to continue with a previous election.
         </p>
       </div>
 
-      <Modal show={openModal} size="md" onClose={() => setOpenModal(false)} popup>
+      <Modal show={openModal} size='md' onClose={() => setOpenModal(false)} popup>
         <Modal.Header />
         <Modal.Body>
-          <form onSubmit={handleSubmit(onCloseModal)}>
-            <div className="space-y-6">
-              <h3 className="text-xl font-medium text-gray-900 dark:text-white">Enter the Election URL</h3>
-              <div>
-                <Controller name='url' control={control}
-                  rules={{
-                    validate: validateURL,
-                  }}
-                  render={({ field }) =>
-                    <TextInput
-                      autoFocus
-                      type="url"
-                      {...field}
-                      color={errors.url && 'failure'}
-                      helperText={
-                        <>
-                          <span className="font-medium">{errors.url?.message!}</span>
-                        </>
-                      }
-                    />} />
-              </div>
-              <div className="w-full">
-                <Button type='submit'>Save Election</Button>
-              </div>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit(onCloseModal)}>
+            <div className='flex flex-col gap-2'>
+              <Label htmlFor='url'>Election URL</Label>
+              <Controller name='url' control={control}
+                rules={{
+                  validate: validateURL,
+                }}
+                render={({ field }) =>
+                  <TextInput
+                    autoFocus
+                    type='url'
+                    {...field}
+                    color={errors.url && 'failure'}
+                    helperText={<span className='font-medium'>{errors.url?.message}</span>}
+                  />} />
+            </div>
+            <div className='flex flex-col gap-2'>
+              <Label htmlFor='key'>Wallet Seed or Viewing Key</Label>
+              <Controller name='key' control={control}
+                rules={{
+                  validate: validateKey,
+                }}
+                render={({ field }) =>
+                  <TextInput
+                    autoFocus
+                    type='key'
+                    {...field}
+                    color={errors.key && 'failure'}
+                    helperText={<span className='font-medium'>{errors.key?.message}</span>}
+                  />} />
+            </div>
+            <div className='w-full gap-8'>
+              <Button type='submit'>Save Election</Button>
             </div>
           </form>
         </Modal.Body>
@@ -148,15 +159,20 @@ async function validateURL(url: string) {
   return true
 }
 
+async function validateKey(key: string) {
+  const isValid: boolean = await invoke('validate_key', {key: key});
+  return isValid || 'Invalid Key. Key must be either a 24 seed phrase or a unified viewing key with an Orchard receiver'
+}
+
 function App() {
   return (
     <Router>
-      <div className="mx-auto flex flex-col min-h-screen">
+      <div className='mx-auto flex flex-col min-h-screen'>
         <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<Election />} />
-          <Route path="/overview" element={<Overview />} />
-          <Route path="/contact" element={<div />} />
+          <Route path='/' element={<Navigate to='/home' />} />
+          <Route path='/home' element={<Election />} />
+          <Route path='/overview' element={<Overview />} />
+          <Route path='/contact' element={<div />} />
         </Routes>
       </div>
     </Router>
