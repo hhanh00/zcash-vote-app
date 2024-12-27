5a983cc808e2c5bd1e5f1d79d5bd9a9fe9bdc427
Hanh <hanh425@gmail.com>
2024-12-27 11:04:42 +1000

    feat: ask and store seed/ufk

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -36,23 +36,31 @@ pub fn create_schema(connection: &Connection) -> Result<()> {
     Ok(())
 }
 
-pub fn store_election(connection: &Connection, url: &str, election: &Election) -> Result<()> {
-    connection.execute(
-        "INSERT INTO properties(name, value) VALUES (?1, ?2)",
-        params!["url", url])?;
-    connection.execute(
-        "INSERT INTO properties(name, value) VALUES (?1, ?2)",
-        params!["election", serde_json::to_string(election).unwrap()])?;
+pub fn store_election(connection: &Connection, url: &str, election: &Election, key: &str) -> Result<()> {
+    store_prop(connection, "url", url)?;
+    store_prop(connection, "election", &serde_json::to_string(election).unwrap())?;
+    store_prop(connection, "key", key)?;
     Ok(())
 }
 
-pub fn load_election(connection: &Connection) -> Result<(String, Election)> {
-    let url = connection.query_row(
-        "SELECT value FROM properties WHERE name = ?1", ["url"],
-    |r| r.get::<_, String>(0))?;
-    let election = connection.query_row(
-        "SELECT value FROM properties WHERE name = ?1", ["election"],
-    |r| r.get::<_, String>(0))?;
+pub fn load_election(connection: &Connection) -> Result<(String, Election, String)> {
+    let url = load_prop(connection, "url")?;
+    let election = load_prop(connection, "election")?;
+    let key = load_prop(connection, "key")?;
     let election: Election = serde_json::from_str(&election)?;
-    Ok((url, election))
+    Ok((url, election, key))
+}
+
+fn store_prop(connection: &Connection, name: &str, value: &str) -> Result<()> {
+    connection.execute(
+        "INSERT INTO properties(name, value) VALUES (?1, ?2)",
+        params![name, value])?;
+    Ok(())
+}
+
+fn load_prop(connection: &Connection, name: &str) -> Result<String> {
+    let value: String = connection.query_row(
+        "SELECT value FROM properties WHERE name = ?1", [name],
+    |r| r.get::<_, String>(0))?;
+    Ok(value)
 }
