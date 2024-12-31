96399468d27a3de5295f478a79cbdb1251343c1f
Hanh <hanh425@gmail.com>
2024-12-31 17:38:53 +1000

    chore: move vote to zcash-vote

--- src-tauri/src/address.rs
+++ src-tauri/src/address.rs
@@ -1,49 +1,11 @@
-use std::{fmt::{Display, Formatter}, sync::Mutex};
+use std::sync::Mutex;
 
 use anyhow::{Error, Result};
-use orchard::{keys::Scope, Address};
-use bech32::{FromBase32, ToBase32};
+use orchard::keys::Scope;
 use tauri::State;
-use zcash_vote::decrypt::to_fvk;
+use zcash_vote::{address::VoteAddress, decrypt::to_fvk};
 
 use crate::state::AppState;
-
-pub struct VoteAddress(pub Address);
-
-const VOTE_HRP: &str = "zvote";
-
-impl VoteAddress {
-    pub fn decode(s: &str) -> Result<Self> {
-        let (hrp, data, _) = bech32::decode(&s)?;
-        if &hrp != VOTE_HRP {
-            anyhow::bail!("Invalid Address (incorrect prefix)");
-        }
-        let data = Vec::<u8>::from_base32(&data)?;
-        if data.len() != 43 {
-            anyhow::bail!("Invalid Address (incorrect length)")
-        }
-        let address = Address::from_raw_address_bytes(&data.try_into().unwrap());
-        if address.is_none().into() {
-            anyhow::bail!("Invalid Address (invalid data)")
-        }
-        let address = address.unwrap();
-        Ok(VoteAddress(address))
-    }
-
-    pub fn encode(&self) -> String {
-        let address = &self.0;
-        let address = address.to_raw_address_bytes();
-        let address = address.to_base32();
-        let address = bech32::encode(VOTE_HRP, &address, bech32::Variant::Bech32m).unwrap();
-        address
-    }
-}
-
-impl Display for VoteAddress {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.write_str(&self.encode())
-    }
-}
 
 #[tauri::command]
 pub fn get_address(state: State<Mutex<AppState>>) -> Result<String, String> {
